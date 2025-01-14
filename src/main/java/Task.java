e9ca65a7c0aff029081d14932049b5c3eba7d3b5
Wrongian <higgsbosonsandquarks@gmail.com>
2025-01-14 19:34:16 +0800

    Added the Task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,12 @@
+public class Task {
+
+    private final String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,17 +1,18 @@
 import java.lang.StringBuilder;
 public class TaskList {
     private static final int MAX_TASKS = 100;
-    private String[] tasks;
+    private final Task[] tasks;
     private int taskIndex;
 
     public TaskList() {
-        this.tasks = new String[TaskList.MAX_TASKS];
+        this.tasks = new Task[TaskList.MAX_TASKS];
         this.taskIndex = 0;
     }
 
-    public void addTask(String task) {
+    public void addTask(String name) {
+        Task newTask = new Task(name);
         // modify task array
-        this.tasks[taskIndex] = task;
+        this.tasks[taskIndex] = newTask;
         this.taskIndex += 1;
     }
 
@@ -20,7 +21,7 @@ public class TaskList {
         for (int i = 0; i < this.taskIndex; i++) {
             msg.append(String.valueOf(i + 1));
             msg.append(". ");
-            msg.append(this.tasks[i]);
+            msg.append(this.tasks[i].getName());
             msg.append("\n");
         }
         // delete last newline
