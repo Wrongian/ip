f095f3538bd4ba05c96a342c02ff13465299d338
Wrongian <higgsbosonsandquarks@gmail.com>
2025-01-14 17:58:43 +0800

    Adding the adding and listing task feature

--- src/main/java/Demacia.java
+++ src/main/java/Demacia.java
@@ -1,10 +1,45 @@
+import java.lang.StringBuilder;
 public class Demacia {
 
+    private static final int MAX_TASKS = 100;
+
     private final Terminal terminal;
+    private String[] tasks;
+    private int taskIndex;
 
     public Demacia() {
         this.terminal = new Terminal();
+        this.tasks = new String[Demacia.MAX_TASKS];
+        this.taskIndex = 0;
     }
+
+    public void addTask(String task) {
+        // modify task array
+        this.tasks[taskIndex] = task;
+        this.taskIndex += 1;
+
+        // msg
+        this.terminal.printHorizontal();
+        String msg = "added: " + task;
+        this.terminal.output(msg);
+        this.terminal.printHorizontal();
+    }
+
+    public void listTasks() {
+        this.terminal.printHorizontal();
+        StringBuilder msg = new StringBuilder();
+        for (int i = 0; i < this.taskIndex; i++) {
+            msg.append(String.valueOf(i + 1));
+            msg.append(". ");
+            msg.append(this.tasks[i]);
+            msg.append("\n");
+        }
+        // delete last newline
+        msg.deleteCharAt(msg.length() - 1);
+        this.terminal.output(msg.toString());
+        this.terminal.printHorizontal();
+    }
+
     public void greet() {
         this.terminal.printHorizontal();
         this.terminal.output("Hello I am Demacia, a chatbot");
@@ -19,11 +54,13 @@ public class Demacia {
             switch (msg) {
                 case "bye":
                     this.exit();
+                    break;
+                case "list":
+                    this.listTasks();
+                    break;
                 default:
-                    // echo by default for now
-                    this.terminal.printHorizontal();
-                    this.terminal.output(msg);
-                    this.terminal.printHorizontal();
+                    addTask(msg);
+                    break;
             }
         }
 
