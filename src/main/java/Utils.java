d522f2f12ccc706d4068001734aeb84432142a32
MSI\Ian <higgsbosonsandquarks@gmail.com>
2025-02-01 03:06:02 +0800

    Delete unused functions in Utils class
    The method joinStringArray() is unused.
    
    Unused methods should be removed.
    
    Remove the joinStringArray() method from the Utils class.
    
    This would make it easier for other developers to read.

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -1,20 +1,6 @@
 import java.lang.StringBuilder;
 
 public class Utils {
-    public static String joinStringArray(String[] arr, int start, int end) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = start; i < end + 1; i++) {
-           sb.append(arr[i]);
-           sb.append(" ");
-        }
-        sb.deleteCharAt(sb.length() - 1);
-        return sb.toString();
-    }
-
-    public static String joinStringArray(String[] arr, int start) {
-        return joinStringArray(arr, start, arr.length - 1);
-    }
-
     public static boolean stringIsInt(String str) {
         // empty string
         if (str.isEmpty()) {
