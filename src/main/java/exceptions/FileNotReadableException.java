bfd8d9db02d6ec3b8f30f064fcc6574065968fb5
MSI\Ian <higgsbosonsandquarks@gmail.com>
2025-02-01 01:50:36 +0800

    Revert "Add file handler class"
    This reverts commit a0e400c767e2f4ef55d040a9354314270760b6ff.

--- src/main/java/exceptions/FileNotReadableException.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-public class FileNotReadableException extends DukeException {
-
-    public FileNotReadableException(String filePath) {
-        super("File at path " + filePath + " is not readable\nPlease enable the file to be readable");
-    }
-}
